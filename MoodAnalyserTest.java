7ddb478ae58b872d5a551dd2075afec303bf191f
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-07-21 08:55:36 +0530

    Exception Handling with Mood Analyser

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,5 +1,26 @@
 package com.BL.in;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class MoodAnalyserTest {
+ @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("Sad", mood);
+    }
 
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        Assert.assertEquals("Happy", mood);
+    }
+    @Test
+    public void givenNullMood_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyzeMood();
+        Assert.assertEquals("Happy", mood);
+    }
 }
